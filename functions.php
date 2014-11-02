47fc8b114a087e7ff1c1c5cfd0185e036d4a21f7
Bas Grolleman <bgrolleman@emendo-it.nl>
2014-11-03 00:06:03 +0100

    Update Theme Name

--- functions.php
+++ functions.php
@@ -1,11 +1,7 @@
 <?php
 /*
-Author: Eddie Machado
-URL: htp://themble.com/bones/
-
-This is where you can drop your custom functions or
-just edit things like thumbnail sizes, header images,
-sidebars, comments, ect.
+Author: Bas Grolleman
+URL: https://bitbucket.org/bgrolleman/abunai-wordpress-theme
 */
 
 // LOAD BONES CORE (if you remove this, the theme will break)
